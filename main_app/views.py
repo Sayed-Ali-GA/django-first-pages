68232518d0f10cf69313db125752cf87de8931c9
sayed-ali-ga <alsayed3li2005@gmail.com>
2025-08-20 11:01:45 +0300

    Finl 1

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    context = {
+
+        "title": 'My App'
+    }
+
+
+    return render(request, 'home.html', context)
+
+
+def about(request):
+    conttext = {
+        "title": 'About page'
+    }
+
+    return render(request, 'about.html', conttext)
